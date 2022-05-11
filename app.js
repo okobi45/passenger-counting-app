c3259296fa0d8b089074df8b24cbaab5f9c0bd1f
Okobi Eigbe <okobieigbe@gmail.com>
2022-05-11 23:50:07 +0100

    new file

---
+++ app.js
@@ -0,0 +1,8 @@
+
+const newPass = document.getElementById("new-passenger");
+const passRec = document.getElementById("passenger-record");
+count = 0;
+
+function addPass() {
+    console.log("i was clicked")
+}
